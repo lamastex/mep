d3da4c951e0a95e26bd765d1aa3abfd2b0f0c80c
Johannes Graner <johannes.graner@gmail.com>
2021-07-22 08:23:52 +0200

    Terraform implementation for Twitter stream on AWS

---
+++ infra/tf/server/aws_security.tf
@@ -0,0 +1,29 @@
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "ssh-key"
+  public_key = file("${path.module}/credentials/aws-key.pub")
+}
+
+resource "aws_security_group" "group" {
+  name = "group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
